b7e69dc6c17891f3b8c7b0f87ba17107b9b42778
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-07-25 13:38:16 +0300

    Added more ways of emitting initialized library.

--- app/src/main/java/com/szagurskii/superfaststartup/splash/SplashModule.java
+++ app/src/main/java/com/szagurskii/superfaststartup/splash/SplashModule.java
@@ -14,12 +14,12 @@ import rx.Observable;
 import rx.Single;
 import rx.functions.Action1;
 import rx.functions.Func0;
-import rx.schedulers.Schedulers;
 
 @Module
 public class SplashModule {
   public static final String SPLASH_ACTIVITY = "SplashActivity";
   public static final String OBSERVABLE_SPLASH_LIBRARY = "observable_splash_library";
+  public static final String OBSERVABLE_SPLASH_LIBRARY_FROM_CALLABLE = "observable_splash_library_from_callable";
   public static final String OBSERVABLE_SPLASH_LIBRARY_ASYNC = "observable_splash_library_async";
 
   @Provides @NonNull @SplashScope @Named(SPLASH_ACTIVITY)
@@ -41,17 +41,27 @@ public class SplashModule {
     });
   }
 
-  /** Other possible way to initialize {@link SplashLibrary}. */
+  /** Another possible way to emit initialized {@link SplashLibrary}. */
+  @Provides @NonNull @SplashScope @Named(OBSERVABLE_SPLASH_LIBRARY_FROM_CALLABLE)
+  public Observable<SplashLibrary> splashLibraryObservableFromCallable(final Lazy<SplashLibrary> splashLazy) {
+    return Observable.fromCallable(new Func0<SplashLibrary>() {
+      @Override public SplashLibrary call() {
+        return splashLazy.get();
+      }
+    });
+  }
+
+  /** Another possible way to emit initialized {@link SplashLibrary}. */
   @Provides @NonNull @SplashScope @Named(OBSERVABLE_SPLASH_LIBRARY_ASYNC)
   public Observable<SplashLibrary> splashLibraryObservableAsync(final Lazy<SplashLibrary> splashLazy) {
     return Observable.fromAsync(new Action1<AsyncEmitter<SplashLibrary>>() {
       @Override public void call(AsyncEmitter<SplashLibrary> emitter) {
         emitter.onNext(splashLazy.get());
       }
-    }, AsyncEmitter.BackpressureMode.NONE).subscribeOn(Schedulers.computation());
+    }, AsyncEmitter.BackpressureMode.NONE);
   }
 
-  /** Other possible way to initialize {@link SplashLibrary}. */
+  /** Another possible way to emit initialized {@link SplashLibrary}. */
   @Provides @NonNull @SplashScope
   public Single<SplashLibrary> splashLibrarySingle(final Lazy<SplashLibrary> splashLazy) {
     return Single.defer(new Func0<Single<SplashLibrary>>() {
